5c4e09cf3caa7fd717e6f773c6be0a655fa2ca04
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-06-11 08:52:05 +0700

    pushed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { response } = require("express");
 const morgan = require('morgan')
 const express = require("express");
+const cors = require('cors')
+
 const app = express();
+app.use(cors())
 
 let persons = [
   {
