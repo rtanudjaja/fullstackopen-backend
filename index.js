e8199a693d4be051cd8cc0368a855f7da872960d
Ricky Tanudjaja <rickytanudjaja95@gmail.com>
2022-06-12 14:16:26 +0700

    update phonebook database

--- index.js
+++ index.js
@@ -33,9 +33,7 @@ app.get("/api/persons", (req, res) => {
     .then((persons) => {
       res.json(persons);
     })
-    .catch(() => {
-      res.status(404).end();
-    });
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (req, res) => {
@@ -47,75 +45,70 @@ app.get("/api/persons/:id", (req, res) => {
         res.status(404).end();
       }
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(500).end();
-    });
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (req, res) => {
   Person.findByIdAndRemove(req.params.id)
-    .then(result => {
-      response.status(204).end()
+    .then((result) => {
+      res.status(204).end();
     })
-    .catch(error => next(error))
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (req, res) => {
-  const uniqueId = Math.floor(Math.random() * 1000000);
-  const newPerson = req.body;
-  newPerson.id = uniqueId;
+  const body = req.body;
 
-  if (!newPerson.name && !newPerson.number) {
-    return res.status(400).json({
-      error: "The name and number are missing",
-    });
-  } else if (!newPerson.name) {
-    return res.status(400).json({
-      error: "The name is missing",
-    });
-  } else if (!newPerson.number) {
-    return res.status(400).json({
-      error: "The number is missing",
-    });
-  } else if (
-    persons.findIndex((person) => person.name === newPerson.name) > -1
-  ) {
-    return res.status(400).json({
-      error: "The name already exists in the phonebook",
-    });
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: "name and/or number is missing" });
   }
 
-  persons = persons.concat(newPerson);
-  res.json(newPerson);
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => savedPerson.toJSON())
+    .then((savedAndFormattedPerson) => {
+      res.json(savedAndFormattedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (req, res) => {
-  res.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date().toString()}</p>
-  `);
+  Person.find({})
+    .then((persons) => {
+      res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date().toString()}</p>
+      `);
+    })
+    .catch(() => {
+      res.status(404).end();
+    });
 });
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
 };
 
-app.use(unknownEndpoint);
-
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
-  next(error)
-}
+  next(error);
+};
 
-app.use(errorHandler)
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
